66930db4d6f800d4346b21516c4f6b92a7d04003
Alexander Turok <alexander.turok2004@gmail.com>
2022-08-15 15:57:24 -0500

    create redis handlers

---
+++ pkg/telegram/redis.go
@@ -0,0 +1,18 @@
+package telegram
+
+import "fmt"
+
+func (b *Bot) setCache(key int, value string) error {
+	err := b.redis.Set(b.ctx, fmt.Sprint(key), value, 0)
+	return err.Err()
+}
+
+func (b *Bot) getCache(key int) string {
+	value, _ := b.redis.Get(b.ctx, fmt.Sprint(key)).Result()
+	return value
+}
+
+func (b *Bot) deleteCache(key int) error {
+	err := b.redis.Del(b.ctx, fmt.Sprint(key))
+	return err.Err()
+}
