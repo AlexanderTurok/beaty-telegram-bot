1ecae17aa993025195bba64a3cb4a53a6f4a07d0
Alexander Turok <alexander.turok2004@gmail.com>
2022-08-15 16:17:25 -0500

    correct participant struct

--- pkg/telegram/participant.go
+++ pkg/telegram/participant.go
@@ -1,10 +1,10 @@
 package telegram
 
 type Participant struct {
-	Id          int
-	Uuid        int
-	Nickname    string
-	Photo       string
-	Information string
-	Votes       int
+	Id          int    `db:"id"`
+	Uuid        int    `db:"uuid"`
+	Nickname    string `db:"nickname"`
+	Photo       string `db:"photo"`
+	Information string `db:"information"`
+	Votes       int    `db:"votes"`
 }
