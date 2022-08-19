9b1564863c837722ade74c12b2fcd1d7524df0ed
Alexander Turok <alexander.turok2004@gmail.com>
2022-08-19 11:52:27 -0500

    implement changes to main

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"context"
-	"database/sql"
-	"fmt"
 	"log"
 	"os"
 
-	"github.com/AlexanderTurok/telegram-beaty-bot/pkg/telegram"
-	"github.com/go-redis/redis/v9"
+	"github.com/AlexanderTurok/telegram-beaty-bot/pkg/repository"
+	"github.com/AlexanderTurok/telegram-beaty-bot/pkg/service"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 )
 
 func main() {
@@ -22,25 +21,40 @@ func main() {
 		log.Fatalf("error while loading env: %s", err.Error())
 	}
 
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+
+	redis := repository.NewRedis(ctx, repository.Config{
+		Host:     viper.GetString("redis.address"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+	})
+
 	botApi, err := tgbotapi.NewBotAPI(os.Getenv("API_KEY"))
 	if err != nil {
 		log.Fatalf("error while getting api key: %s", err.Error())
 	}
 
-	botApi.Debug = true
+	botApi.Debug = false
 
-	postgres, err := sql.Open("postgres", fmt.Sprintf("dbname=%s sslmode=disable", os.Getenv("DB_NAME")))
-	if err != nil {
-		log.Fatalf("error while openig postgres: %s", err.Error())
-	}
-
-	redis := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_ADDRESS"),
-		Password: "",
-		DB:       0,
-	})
-
-	bot := telegram.NewBot(botApi, redis, postgres, ctx)
+	repository := repository.NewRepository(ctx, db, redis)
+	service := service.NewService(repository)
+	bot := bot.NewBot(botApi, service)
 
 	bot.Start()
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
