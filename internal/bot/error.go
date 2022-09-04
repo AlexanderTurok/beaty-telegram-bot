a6e5855135cc924c3356a8eceb5bec3e28b11959
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-04 16:01:37 -0500

    create handler for errors

---
+++ internal/bot/error.go
@@ -0,0 +1,10 @@
+package bot
+
+import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+
+func (b *Bot) handleError(userId int64, err error) error {
+	msg := tgbotapi.NewMessage(userId, "Error: "+err.Error())
+	_, err = b.bot.Send(msg)
+
+	return err
+}
