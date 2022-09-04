09183060b6484467b6fef5c93ea05fc37af2b9df
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-04 16:38:58 -0500

    implement Create; GetParticipant

--- internal/service/voter.go
+++ internal/service/voter.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"github.com/AlexanderTurok/telegram-beaty-bot/internal/repository"
+	telegram "github.com/AlexanderTurok/telegram-beaty-bot/pkg"
 )
 
 type VoterService struct {
@@ -12,4 +13,29 @@ func NewVoterService(repository repository.Voter) *VoterService {
 	return &VoterService{
 		repository: repository,
 	}
+}
+
+func (s *VoterService) Create(uuid int64) error {
+	exists, err := s.repository.IsExists(uuid)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return s.repository.Create(uuid)
+	}
+
+	return nil
+}
+
+func (s *VoterService) GetParticipant(uuid int64) (telegram.Participant, error) {
+	participant, err := s.repository.GetParticipant(uuid)
+	if err != nil {
+		return telegram.Participant{}, err
+	}
+
+	if err := participant.Validate(); err != nil {
+		return telegram.Participant{}, err
+	}
+
+	return participant, err
 }
