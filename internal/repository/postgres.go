ecf389950a9d5c285b33d044c9eaa53067925eb1
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-15 20:11:53 -0500

    upd

--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -1,13 +1,14 @@
 package repository
 
 import (
+	"fmt"
+
 	"github.com/jmoiron/sqlx"
 )
 
 func NewPostgresDB(config Config) (*sqlx.DB, error) {
-	// db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
-	// 	config.Host, config.Port, config.Username, config.DBName, config.Password, config.SSLMode))
-	db, err := sqlx.Open("postgres", "dbname=tgbeaty_db sslmode=disable")
+	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+		config.Host, config.Port, config.Username, config.DBName, config.Password, config.SSLMode))
 	if err != nil {
 		return nil, err
 	}
