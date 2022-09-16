079ea39a7b1b751b139d8b573a98c376a68156ce
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-15 19:36:04 -0500

    change error handling for user

--- internal/bot/error.go
+++ internal/bot/error.go
@@ -3,7 +3,7 @@ package bot
 import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 
 func (b *Bot) handleError(userId int64, err error) error {
-	msg := tgbotapi.NewMessage(userId, "Error: "+err.Error())
+	msg := tgbotapi.NewMessage(userId, err.Error())
 	_, err = b.bot.Send(msg)
 
 	return err
